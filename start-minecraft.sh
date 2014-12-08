9dcc5cab7e0c649547211275fa6040fc82528411
Alex Varju <alex@varju.ca>
2014-12-07 17:42:22 -0800

    better model for persisting data while allowing server to be upgraded

--- start-minecraft.sh
+++ start-minecraft.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-cd /minecraft
-
-# sync the installed files over to persistent storage
-if [ ! -f PocketMine-MP.phar ]; then
-  cp -rp /minecraft-installer/* .
-fi
+# link to persistent versions of data files
+cd /minecraft-installer
+for dir in players worlds plugins; do
+  mkdir -p /minecraft/$dir
+  ln -s /minecraft/$dir
+done
+for file in white-list.txt ops.txt pocketmine.yml banned-players.txt banned-ips.txt server.properties; do
+  ln -s /minecraft/$file
+done
 
 ./start.sh --no-wizard
